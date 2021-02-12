873b15921e9260c8dba25b0580c17e67ed15df79
Joshua Emmons <skia@skia.net>
2021-02-12 12:04:21 -0600

    WIP

---
+++ Sources/NeolithRoutes/Routes/Routes.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Neolith
+
+
+
+// MARK: - INIT
+public class Routes {
+//  private let routes: [Route.Type]
+
+//
+//  lazy public var pipe: InputPipe = { request, FileIO, completion in
+//    for routeFactory in self.routes {
+//      if let route = routeFactory.init(request: request) {
+//        route.response { completion(.writeOut($0)) }
+//        return
+//      }
+//    }
+//    completion(.passUp(request))
+//  }
+//
+//
+//  public init(_ someRoutes: [Route.Type]) {
+//    routes = someRoutes
+//  }
+}
---
+++ Sources/NeolithRoutes/Routes/Static.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Neolith
+
+
+
+//public struct Static: Route {
+//  private let path: String
+//
+//
+//  public init?(request: NeolithRequest) {
+//    path = request.path
+//  }
+//
+//
+//  public func response(completion: (NeolithResponse) -> Void) {
+//    completion(
+//      NeolithResponse(
+//        status: .ok,
+//        headers: [:],
+//        file: URL(fileURLWithPath: path)
+//      )
+//    )
+//  }
+//}
